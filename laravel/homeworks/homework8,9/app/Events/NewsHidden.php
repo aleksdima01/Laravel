e18d272ab3da426023d1e946c189d91e9c980374
Dmitrii <aleksdima@mail.ru>
2024-12-09 11:23:44 +0300

    Homework 9 completed

---
+++ laravel/homeworks/homework8,9/app/Events/NewsHidden.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\News;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class NewsHidden
+{
+    public News $news;
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(News $news)
+    {
+        $this->news = $news;
+    }
+}
--- laravel/homeworks/homework8/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\LogController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/logs', [LogController::class, 'showLogs']);
